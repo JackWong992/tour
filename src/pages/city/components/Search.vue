40041815264de9fd8c4d06aae771430777caa6d8
JackWong992 <jackwong992@163.com>
2018-07-24 16:41:08 +0800

    search to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input type="text" 
+            placeholder="输入城市名或者拼音"
+            class="search-input">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CitySearch'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../../assets/styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem  
+    background: $bgColor
+    .search-input
+      height: .62rem
+      line-height: .62rem
+      text-align: center
+      width: 100%
+      border-radius: .06rem
+      color: #666
+      padding: 0 .1rem
+      box-sizing: border-box
+</style>
